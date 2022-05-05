6a8d94e9b50c8430554373d59fb8f3a4194a305a
Marcos8060 <marcosgav80@gmail.com>
2022-05-05 15:14:37 +0300

    made minor changes

--- shop_api/models.py
+++ shop_api/models.py
@@ -11,13 +11,12 @@ class Item(models.Model):
     price = models.CharField(max_length=10)
     amount = models.IntegerField(default=1)
 
-
     def __str__(self):
         return self.name
 
 
 class Soon(models.Model):
-    image = CloudinaryField('image')
+    image = models.ImageField(upload_to='images') # image
     name = models.CharField(max_length=80)
     description = models.TextField()
     category = models.CharField(max_length=30)
